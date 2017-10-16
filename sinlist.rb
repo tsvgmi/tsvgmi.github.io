5a229d32ef9cd3e85de1e98501e923fabf529d65
Thien Vuong <thien.vuong@etrade.com>
2017-10-15 18:06:46 -0700

    Add some sample intro

--- sinlist.rb
+++ sinlist.rb
@@ -2,33 +2,49 @@
 require File.dirname(__FILE__) + "/etc/toolenv"
 require 'sinatra'
 require 'sinatra/content_for'
+require 'sinatra/reloader'
+require 'sinatra/partial'
 require 'yaml'
+require 'net/http'
 require 'core'
 
 #set :bind, '0.0.0.0'
 
 get '/' do
-  haml :index
+  redirect "/program/vnhv-thu-2017"
 end
 
 get '/list/:event' do |event|
   haml :list, locals: {event: event}, layout:nil
 end
 
-get '/content/:event' do |event|
-  plist = load_plist(event)
-  haml :content, locals: {plist:plist}
-end
-
 get '/program/:event' do |event|
-  plist = load_plist(event)
-  haml :program, locals: {plist:plist}
+  ord_list   = YAML.load_file("#{event}.order")
+  song_store = load_songs(event)
+  performers = []
+  styles     = []
+  ord_list.each_with_index do |asection, sec_no|
+    asection['list'].each do |aname|
+      sentry = song_store[aname]
+      next unless sentry
+      if sentry[:performer]
+        performers += sentry[:performer].split(/\s*,\s*/)
+      end
+      if sentry[:pstyle]
+        styles << sentry[:pstyle].downcase
+      end
+    end
+  end
+  performers = performers.sort.uniq
+  styles     = styles.sort.uniq
+  haml :program, locals: {ord_list:ord_list, song_store:song_store,
+                          performers:performers, styles:styles}
 end
 
-get '/master/:event' do |event|
-  ord_list   = YAML.load_file("#{event}.order2")
+get '/knockout/:event' do |event|
+  ord_list   = YAML.load_file("#{event}.order")
   song_store = load_songs(event)
-  haml :master, locals: {ord_list:ord_list, song_store:song_store}
+  haml :knockout, locals: {ord_list:ord_list, song_store:song_store}
 end
 
 get '/send_patch/:pstring' do |pstring|
@@ -37,17 +53,52 @@ get '/send_patch/:pstring' do |pstring|
   haml :patch_info, locals: {presult:presult}, layout:nil
 end
 
-helpers do
-  def load_plist(event)
-    p2list = load_songs(event)
-    order_list = File.read("#{event}.order").split("\n")
-    order_list.map{|e| [e, p2list[e]]}
-  end
+get '/show_lyric' do
+  url   = params[:url]
+  #lfile = "data/" + url.gsub('/', '#')
+  #unless test(?s, lfile)
+  #content = Net::HTTP.get(URI.parse(url))
+  lyric_info(url).to_json
+end
 
+helpers do
   def load_songs(event)
     plist = YAML.load_file("#{event}.slist").each do |e|
       e[:sname] = e[:href] ? e[:href].split('/')[5] : e[:name].downcase
             end
     Hash[plist.map{|e| [e[:sname], e]}]
   end
+
+  def lyric_info(url)
+    Plog.info("Extract lyrics from #{url}")
+    lfile = "data/#{url.sub(/\/$/, '').gsub('/', '#')}"
+    if test(?s, lfile)
+      return YAML.load_file(lfile)
+    else
+      page   = get_page(url)
+    end
+    lyric  = page.css('.chord_lyric_line').map{|r| r.text.strip}.join("\n").
+              strip.gsub(/ \]/, ']')
+    artist = page.css('.perform-singer-list').map {|r| r.text.strip}
+    ret = {
+      title:  page.css('#song-title').text.strip,
+      artist: artist.join(', '),
+      lyric:  lyric,
+    }
+    File.open(lfile, "w") do |fod|
+      fod.puts ret.to_yaml
+    end
+    ret
+  end
+
+  def get_page(url)
+    require 'open-uri'
+    require 'nokogiri'
+
+    fid  = open(url)
+    page = Nokogiri::HTML(fid.read)
+    fid.close
+    page
+  end
+
 end
